020c805488191208ab6d0ed9d15bc974564e7a06
Jimmy <chen.jimmym@gmail.com>
2017-09-26 19:50:25 -0700

    almost done

---
+++ ninjacat.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //alert("ready");
+    
+    $(".one").click(function(){
+        $(".one").toggle(800);
+    })
+
+    $(".two").click(function(){
+        $(".two").toggle(800);
+    })
+
+    $(".three").click(function(){
+        $(".three").toggle(800);
+    })
+
+    $(".four").click(function(){
+        $(".four").toggle(800);
+    })
+
+    $(".five").click(function(){
+        $(".five").toggle(800);
+    })
+
+});
+
+
+
